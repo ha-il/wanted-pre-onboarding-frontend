aa4ca5c92efd056cef508cb58cfd659c7d7969fd
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-08-04 17:04:54 +0900

    chore: Setup Emotion

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: ['@emotion'],
+  presets: [
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+        importSource: '@emotion/react',
+      },
+    ],
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+  ],
+};
